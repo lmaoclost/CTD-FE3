513e3e5f692ccc4dbb1327473d8cdd362105bf82
lmaoclost <renansmoliveira@gmail.com>
2021-10-17 15:54:20 -0300

    Subiu aula 1

---
+++ Aula/aula-01/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Essa é minha primeira aula de React
+      </header>
+    </div>
+  );
+}
+
+export default App;
