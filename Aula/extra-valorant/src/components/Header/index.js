bd3e0ca56731f440823824b83962223d6b28e835
lmaoclost <renansmoliveira@gmail.com>
2021-10-25 09:46:30 -0300

    Ajustou header valorant

--- Aula/extra-valorant/src/components/Header/index.js
+++ Aula/extra-valorant/src/components/Header/index.js
@@ -3,11 +3,11 @@ import './style.css';
 const Header = () => {
 
   return(
-    <header>
+    <header id="header">
       <h1>Valorant do Amor</h1>
       <nav>
         <ul>
-          <li><a href="#">Valorant</a></li>
+          <li><a href="#header">Valorant</a></li>
           <li><a href="#galeria">Videos</a></li>
           <li><a href="#footer">Footer</a></li>
         </ul>
