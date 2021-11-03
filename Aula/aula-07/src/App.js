2eee7b5e848aa009d2b9f18728da088d7a334d53
lmaoclost <renansmoliveira@gmail.com>
2021-11-03 11:22:21 -0300

    Ajustou aula 07

--- Aula/aula-07/src/App.js
+++ Aula/aula-07/src/App.js
@@ -4,11 +4,6 @@ import Video from './components/Video';
 import './App.scss';
 
 function App() {
-  const user = {
-    firstName: 'Turma',
-    lastName: 'XX'
-  }
-
   const listagemDeAlunos = [
     {
       firstName: 'João',
@@ -24,64 +19,58 @@ function App() {
     },
     {
       firstName: 'Ciclano',
-      lastName: 'Betrano'
+      lastName: 'Beltrano'
     }
   ]
 
   return (
     <>
-      <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Expand at sm</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Expand at sm</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarsExample03">
-            <ul class="navbar-nav me-auto mb-2 mb-sm-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <div className="collapse navbar-collapse" id="navbarsExample03">
+            <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                <ul class="dropdown-menu" aria-labelledby="dropdown03">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                <ul className="dropdown-menu" aria-labelledby="dropdown03">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
                 </ul>
               </li>
             </ul>
             <form>
-              <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
+              <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
             </form>
           </div>
         </div>
       </nav>
       <div className="App">
-        <Greeting user={user} />
-        <Video url="https://www.youtube.com/embed/l04_GGlr3BI">
-          Parabains {user.firstName}.
-        </Video>
         {
-          listagemDeAlunos.map((aluno) => {
+          listagemDeAlunos.map((aluno, index) => {
             return (
-              <Greeting key={aluno.firstName} user={aluno} />
+              <>
+                <Greeting key={index} user={aluno} />
+                <Video url="https://www.youtube.com/embed/l04_GGlr3BI">
+                  Parabains {aluno.firstName}.
+                </Video>
+              </>
             )
           })
         }
-        {listagemDeAlunos.map(({ firstName }, index) => {
-          return (
-            <Video key={index} url="https://www.youtube.com/embed/l04_GGlr3BI">
-              Parabains {firstName}.
-            </Video>
-          )
-        })}
       </div>
     </>
   );
