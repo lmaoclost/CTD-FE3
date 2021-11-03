66327050e25726e92fcb7c8524cc56962081b748
lmaoclost <renansmoliveira@gmail.com>
2021-11-03 08:27:58 -0300

    Subiu aula 07

--- Aula/aula-07/src/App.js
+++ Aula/aula-07/src/App.js
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import Greeting from './components/Greeting';
 import Video from './components/Video';
-import './App.css';
+import './App.scss';
 
 function App() {
   const user = {
@@ -12,7 +13,7 @@ function App() {
     {
       firstName: 'João',
       lastName: 'Silva'
-    }, 
+    },
     {
       firstName: 'Fulano',
       lastName: 'Alberto'
@@ -21,9 +22,37 @@ function App() {
       firstName: 'John',
       lastName: 'Doe'
     }];
-  
+
   return (
     <>
+      <header class="sticky-top">
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">DH Extra</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03"
+              aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarsExample03">
+              <ul class="navbar-nav mb-2 mb-sm-0">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown"
+                    aria-expanded="false">Cursos</a>
+                  <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                    <li><a class="dropdown-item" href="#cursos">CTD</a></li>
+                    <li><a class="dropdown-item" href="#cursos">FullStack</a></li>
+                    <li><a class="dropdown-item" href="#cursos">Outros</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#form">Contato</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
       <div className="App">
         <Greeting user={user} />
         <Video url="https://www.youtube.com/embed/l04_GGlr3BI">
