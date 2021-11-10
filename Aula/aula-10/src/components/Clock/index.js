6abbb796ffb95d5fe5b0fb575ffc4c9c8ca7e528
lmaoclost <renansmoliveira@gmail.com>
2021-11-10 11:42:06 -0300

    Ajustou aula 10

--- Aula/aula-10/src/components/Clock/index.js
+++ Aula/aula-10/src/components/Clock/index.js
@@ -2,31 +2,47 @@ import { Component } from 'react';
 import './style.scss';
 
 export default class Clock extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
+  constructor() {
+    super();
+    this.state = {
+      date: new Date(),
+      movieList: []
+    }
   }
 
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+    this.timerID = setInterval(() => this.tick(), 1000);
+
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=f6541db294ac187416fae0f1b9effcce&language=pt-BR').then((response) => response.json()).then((response) => {
+      this.setState({ movieList: response.results })
+    });
+  }
+
+  componentDidUpdate() {
   }
 
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
 
-  tick() {
-    this.setState({ date: new Date() });
+  tick = () => {
+    this.setState({ date: new Date() })
   }
 
   render() {
     return (
       <div className="d-flex flex-wrap flex-column justify-content-center align-items-center py-3 my-4" >
-        <h2>São {this.state.date.toLocaleTimeString()}.</h2>
+        <h2>{this.state.date.toLocaleTimeString()}</h2>
+        <ul>
+          {this.state.movieList.length && (
+            this.state.movieList.map(({ id, title }) => {
+              return (
+                <li key={id}>{title}</li>
+              )
+            })
+          )}
+        </ul>
       </div>
-    );
+    )
   }
 }
