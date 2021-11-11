6bc464062ce5dc7de2071a4cefd426b652e1bf61
lmaoclost <renansmoliveira@gmail.com>
2021-11-11 18:11:40 -0300

    Subiu resolução da mesa 11

--- Mesas de trabalho/mesa-09/src/components/App.jsx
+++ Mesas de trabalho/mesa-09/src/components/App.jsx
@@ -1,76 +1,48 @@
 import React, { Component } from 'react'
+import pokedex from '../pokedex.json'
 import "../styles/App.css"
 import Pokedex from './Pokedex';
 
-
-// AREA DE TRABALHO --> Substituir pelo JSON
-const pokedex = [
-  {
-    "id": 1,
-    "name": "Bulbasaur",
-    "type": [
-      "Grass",
-      "Poison"
-    ]
-  },
-  {
-    "id": 2,
-    "name": "Ivysaur",
-    "type": [
-      "Grass",
-      "Poison"
-    ]
-  }]
-// AREA DE TRABALHO -----------------------------------
-
-
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemon: [pokedex[0].name, pokedex[0].id, pokedex[0].type],
+      pokemons: pokedex
+    }
+  }
 
-  // AREA DE TRABALHO --> Usar estrutura de Class Component
-  pokemon = [pokedex[0].name, pokedex[0].id, pokedex[0].type];
-  pokemons = pokedex;
-  // AREA DE TRABALHO -------------------------------------------
-
-
-  update(pokemon) {
-    console.log(pokemon);
-    // Nesta função, o Pokémon visível deve ser atualizado
-    // Não vamos esquecer que se for uma função, devemos vinculá-la... como podemos evitar o uso do vínculo?
+  update = (pokemon) => {
+    this.setState(pokemon)
   }
 
   getPokemon = () => {
-      if (this.pokemon[1]?.toString()?.length === 1) return `00${this.pokemon[1]}`
-      if (this.pokemon[1]?.toString()?.length === 2) return `0${this.pokemon[1]}`
-      if (this.pokemon[1]?.toString()?.length === 3) return `${this.pokemon[1]}`
+    if (this.state.pokemon[1]?.toString()?.length === 1) return `00${this.state.pokemon[1]}`
+    if (this.state.pokemon[1]?.toString()?.length === 2) return `0${this.state.pokemon[1]}`
+    if (this.state.pokemon[1]?.toString()?.length === 3) return `${this.state.pokemon[1]}`
   }
 
   getTypes = () => {
     let phrase;
-    if(this.pokemon[2].length === 1) return this.pokemon[2][0]
-    if(this.pokemon[2].length > 1) {
-     for (let index = 0; index < this.pokemon[2].length; index++) {
-       if(index === 0) phrase = this.pokemon[2][index]
-       if(index !== 0) phrase = `${phrase} and ${this.pokemon[2][index]}`
-     }
-     return phrase
+    if (this.state.pokemon[2].length === 1) return this.state.pokemon[2][0]
+    if (this.state.pokemon[2].length > 1) {
+      for (let index = 0; index < this.state.pokemon[2].length; index++) {
+        if (index === 0) phrase = this.state.pokemon[2][index]
+        if (index !== 0) phrase = `${phrase} and ${this.state.pokemon[2][index]}`
+      }
+      return phrase
     }
   }
 
   render() {
-    /* AREA DE TRABALHO
-      Podemos usar a desestruturação ou modificar diretamente as variáveis ​​this.pokemon por this.state ...
-    */
-
     return (
-      <div style={{display:"flex"}}>
-      <div className="container">
-        <img id="pokemonImg" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${this.getPokemon()}.png`} alt={this.pokemon[0]} />
-        <p id="name">{`${this.pokemon[0]} is a pokemon type ${this.getTypes()}`}</p>
+      <div style={{ display: "flex" }}>
+        <div className="container">
+          <img id="pokemonImg" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${this.getPokemon()}.png`} alt={this.state.pokemon[0]} />
+          <p id="name">{`${this.state.pokemon[0]} is a pokemon type ${this.getTypes()}`}</p>
+        </div>
+        <Pokedex updateParent={this.update} pokemons={this.state.pokemons} />
       </div>
-      <Pokedex updateParent={this.update} pokemons={this.pokemons} />
-    </div>
     )
   }
 }
-
-
