9de6e181285829a097ddf98259d99ef0c7c89fde
lmaoclost <renansmoliveira@gmail.com>
2021-11-11 18:11:16 -0300

    Subiu aula 11

--- Aula/aula-11/src/components/Formulario/index.js
+++ Aula/aula-11/src/components/Formulario/index.js
@@ -1,7 +1,6 @@
-import { Component } from 'react';
+import { Component } from "react";
 import { Formik, Form, Field } from 'formik';
 import Swal from 'sweetalert2';
-import './style.scss';
 
 export default class Formulario extends Component {
   handleSubmit = ({ nome, number }) => {
@@ -10,10 +9,9 @@ export default class Formulario extends Component {
         title: 'Aconteceu um erro',
         icon: 'error',
         text: 'Ih rapaz'
-      })
+      });
     }
     else {
-      console.log(nome);
       Swal.fire({
         title: 'Valores enviados',
         icon: 'success',
@@ -24,15 +22,15 @@ export default class Formulario extends Component {
 
   render() {
     return (
-      <div id="formulario" className="d-flex flex-wrap flex-column justify-content-center align-items-center py-3 my-4 text-center">
+      <>
         <Formik initialValues={{ nome: '', number: 0 }} onSubmit={this.handleSubmit}>
           <Form>
-            <Field type="text" id="nome" name="nome" className="form-control" />
-            <Field type="number" id="number" name="number" className="form-control" />
-            <button className="btn btn-primary my-3" type="submit">Enviar</button>
+            <Field placeholder="Insira seu nome" type="text" name="nome" id="nome" className="form-control" />
+            <Field placeholder="Insira sua idade" type="number" name="number" id="number" className="form-control" />
+            <button type="submit">Enviar</button>
           </Form>
         </Formik>
-      </div>
+      </>
     )
   }
 }
