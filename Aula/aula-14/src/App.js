ff015d29c9c9cf74c066ad918baaf2677e1c1e08
lmaoclost <renansmoliveira@gmail.com>
2021-11-17 15:09:52 -0300

    Subiu aula 14

--- Aula/aula-14/src/App.js
+++ Aula/aula-14/src/App.js
@@ -13,8 +13,12 @@ export default class App extends Component {
   }
 
   async componentDidMount() {
+    this.handleSubmit({ nomeUsuario: 'lmaoclost' });
+  }
+
+  handleSubmit = async ({ nomeUsuario }) => {
     try {
-      const response = await api.get(`/users/lmaoclost/repos`);
+      const response = await api.get(`/users/${nomeUsuario}/repos`);
       this.setState({ repositorios: response.data });
     } catch (error) {
       Swal.fire({
@@ -22,21 +26,6 @@ export default class App extends Component {
         icon: 'error',
         text: error.response.data.message
       });
-    }
-  }
-
-  handleSubmit = async ({ nomeUsuario }) => {
-    if (nomeUsuario !== '') {
-      try {
-        const response = await api.get(`/users/${nomeUsuario}/repos`);
-        this.setState({ repositorios: response.data });
-      } catch (error) {
-        Swal.fire({
-          title: error.response.status,
-          icon: 'error',
-          text: error.response.data.message
-        });
-      }
     }
   }
 
@@ -48,22 +37,20 @@ export default class App extends Component {
             <h2>Procure um usuário do Github</h2>
             <Formik initialValues={{ nomeUsuario: '' }} onSubmit={this.handleSubmit}>
               <Form>
-                <Field placeholder="Insira o nome do usuário" required type="text" name="nomeUsuario" id="nomeUsuario" className="form-control" />
-                <button type="submit" className="btn btn-primary my-4">Pesquisar</button>
+                <Field placeholder="Insira o nome do usuário" required type="text" name="nomeUsuario" id="nomeUsuario" className="form-control my-3" />
+                <button type="submit" className="btn btn-primary">Pesquisar</button>
               </Form>
             </Formik>
             {this.state.repositorios && (
-              <ul>
+              <ol className="list-group list-group-numbered my-3">
                 {this.state.repositorios.map(({ id, name, html_url }) => {
                   return (
                     <li key={id}>
-                      <a target="_blank" href={html_url} rel="noreferrer">
-                        {name}
-                      </a>
+                      <a target="_blank" href={html_url} rel="noreferrer">{name}</a>
                     </li>
                   )
                 })}
-              </ul>
+              </ol>
             )}
           </div>
         </main>
